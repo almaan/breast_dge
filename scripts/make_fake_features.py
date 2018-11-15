d0498c8856bed670e73e40927177b65b257a677a
alma <alma@localhost.localdomain>
2018-11-15 20:54:46 +0100

    added tags

--- scripts/make_fake_features.py
+++ scripts/make_fake_features.py
@@ -12,17 +12,18 @@ import os.path as osp
 
 pth = '/home/alma/ST-2018/CNNp/data/pre-tile-extraction'
 out_pth = '/home/alma/ST-2018/CNNp/DGE/data/test_data'
-sample_name = 'count_data-23209_C1.tsv'
+sample_name = 'count_data-23209_D1.tsv'
 sample_id = (sample_name.split('-')[-1]).split('.')[0]
 #%%
 df = pd.read_csv(osp.join(pth,sample_name), sep = '\t', index_col = 0)
 #%%
-coords = np.array([ x.split('x') for x in df.index ])
+coords = np.array([ x.split('x') for x in df.index  ])
 f1 = np.random.choice(['tumor','non-tumor'], size = coords.shape[0], replace = True, p = [0.7,0.3])
-f2 = np.array(['luma'] * coords.shape[0])
+f2 = np.array(['lumb'] * coords.shape[0])
 f2[f1 == 'non-tumor'] = 'non'
 
 feature_data = dict(x_coord = coords[:,0], y_coord= coords[:,1], f1 = f1, f2 = f2)
 fdf = pd.DataFrame(feature_data)
 fdf.to_csv(osp.join(out_pth, '-'.join(['feature_data','.'.join([sample_id,'tsv'])])), index = False, sep = '\t')
+
 
---
+++ scripts/test_run.sh
@@ -0,0 +1,6 @@
+feature_dir="/home/alma/ST-2018/CNNp/DGE/data/test_data"
+count_dir="/home/alma/ST-2018/CNNp/DGE/data/test_data"
+design_file="/home/alma/ST-2018/CNNp/DGE/data/test_data/design_file_test.dat"
+
+Rscript ./dge_trial_test.r --count_dir $count_dir --feature_dir $feature_dir --design $design_file
+
