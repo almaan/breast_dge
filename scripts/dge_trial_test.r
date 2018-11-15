8a1da14a68aeb5f5097597341ccd61a21f039c13
alma <alma@localhost.localdomain>
2018-11-15 20:47:31 +0100

    updated main

--- scripts/dge_trial_test.r
+++ scripts/dge_trial_test.r
@@ -1,9 +1,11 @@
-library("dplyr")
-library("DESeq2")
-library("scran")
+suppressPackageStartupMessages(library("dplyr"))
+suppressPackageStartupMessages(library("DESeq2"))
+suppressPackageStartupMessages(library("scran"))
 
-library("docstring")
-library("optparse")
+suppressPackageStartupMessages(library("docstring"))
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("futile.logger"))
+
 
 
 extract_names <- function(string){
@@ -14,19 +16,31 @@ extract_names <- function(string){
   return(sample_name[[1]][1])
 }
 
-merge_matrices <- function(M1,M2){
+merge_matrices <- function(M1,M2, na_fill = 0.0){
   #' merge two matrices inplace
+  #' will expand matrix M1 as to include the union of columns from M1 and M2
+  #' All unaccounted genes (not present in a matrix) will be
+  #' assigned a value specified by the user
+  #' 
+  #' Input: 
+  #'    - M1 : matrix with that rows of M2 will be vertical appended to
+  #'    - M2 : matrix that is appended to M1 (vertically)
+  #'    - na_fill : value to fill unspecified counts with. 0.0 is default
+  #'    
+  #' Output: 
+  #'    - M1 : concatenated matrix
+  
   r1_names <- rownames(M1)
   r2_names <- rownames(M2)
   M1 <- bind_rows(M1,M2)
   rownames(M1) <- c(r1_names,r2_names)
-  M1[is.na(M1)] = 0.0
+  M1[is.na(M1)] = na_fill
   return(M1)
 }
 
 
 prepare_counts <-function(main_pth,single_sample){
-  #' 
+  #'
   full_pth <- paste(main_pth,single_sample,sep = "/")
   sample_name <- extract_names(single_sample)
   pasCts <- as.data.frame(read.csv(full_pth,sep = '\t',header = TRUE, row.names = 1))
@@ -35,17 +49,23 @@ prepare_counts <-function(main_pth,single_sample){
 }
 
 prepare_coldata <- function(feature_file_dir,single_sample) {
+  
   sample_name <- extract_names(single_sample)
+  patient_id <- strsplit(sample_name,'-')[1]
+  replicate <- strsplit(sample_name,'-')[2]
+  
   full_pth <- paste(paste(feature_file_dir, paste('feature_data',sample_name,sep='-'), sep = '/'),'tsv', sep = '.')
-  print(full_pth)
-  pasFctr <- as.data.frame(read.csv(full_pth, sep = '\t', header = TRUE))
-  part_1 <- apply(pasFctr[c("x_coord","y_coord")], 1, paste, collapse = "x")
-  rownames(pasFctr) <- sapply(part_1, function(x) paste(sample_name,x,sep='_'))
-  return(pasFctr)
+  
+  cdata <- as.data.frame(read.csv(full_pth, sep = '\t', header = TRUE))
+  cdata['id'] <- patient_id
+  cdata['replicate'] <- replicate
+  
+  stem_name <- apply(cdata[c("x_coord","y_coord")], 1, paste, collapse = "x")
+  
+  rownames(cdata) <- sapply(stem_name, function(x) paste(sample_name,x,sep='_'))
+
+  return(cdata)
 }
-
-
-suppressPackageStartupMessages(library("optparse"))
 
 
 main <- function() {
@@ -64,9 +84,33 @@ main <- function() {
     }
   } 
   
-  print(sample_n(cnt,10))
-  print(sample_n(coldata,10))
-   
+  size_factors <- SingleCellExperiment(list(counts = t(cnt)))
+  size_factors <- computeSumFactors(size_factors)
+  size_factors = size_factors@int_colData@listData$size_factor
+
+  dds <- DESeqDataSetFromMatrix(countData = t(cnt),
+                                colData = coldata,
+                                design = design)
+  
+  
+  sizeFactors(dds) <- size_factors
+  
+  #for easy testing
+  keep <- rowSums(counts(dds)) >= 40
+  
+  dds <- dds[keep,]
+  dds <- DESeq(dds, 
+               parallel = TRUE,
+               BPPARAM = N_WORKERS)
+  
+  res <-results(dds)
+  res <- results(dds,
+                 contrast=c("f1","tumor","non-tumor"))
+  
+  resLFC <- lfcShri
+
+  print(res)
+     
 }
 
 
@@ -91,26 +135,39 @@ parser <- add_option(parser,
 
 # parser <- add_option(parser,
 #                      c("-g", "--gene_list"),
-#                      required = FALSE,
 #                      help = paste(c("list of genes to be included in DGE analysis.",
 #                                     "each row shall contain name of gene.",
 #                                     'if no list provided all genes will be used.'),
 #                                   collapse = " "),
 #                      )
 
-#TODO: Implement design file parsing
 
-# parser <- add_option(parser,
-#                      c("-c", "--design"), 
-#                      required = TRUE,
-#                      help = paste(c("design file to be used in DGE-analysis.",
-#                                     "explictily states what features that from the",
-#                                     "feature files that should be used.",
-#                                     "if non specified all features will be used"),
-#                                   collapse = " "),
-#                       )
+parser <- add_option(parser,
+                     c("-c", "--design"),
+                     help = paste(c("design file to be used in DGE-analysis.",
+                                    "explictily states what features from the",
+                                    "feature files that should be used."),
+                                  collapse = " "),
+                      )
+
+parser <- add_option(parser,
+                     c("-w", "--workers"),
+                     help = paste(c("number of cores/workers to be used.",
+                                    "if non given half of maximum will be used.",
+                                    "must be an integer"),
+                                  collapse = " "),
+                     default = floor(detectCores()/2)
+)
 
 args <- parse_args(parser)
+
+source(args$design)
+
+if (!exists(x = "design")) {
+  print("Please provide a proper design-file")
+  print("Exiting")
+  quit(0,status = 0)
+}
 
 
 
@@ -121,6 +178,7 @@ args <- parse_args(parser)
 #COUNT_DATA_DIR <- args$count_dir
 #FEATURE_FILE_DIR = args$feature_dir
 
+#TODO : Consider wether default option for design should be includeded.
 
 # 
 # stp <- TRUE
@@ -132,35 +190,10 @@ if (!interactive()) {
   
   COUNT_DATA_DIR <<- args$count_dir
   FEATURE_FILE_DIR <<- args$feature_dir
+  N_WORKERS <<- args$workers
   main()
   #  SELECTED_GENES <- args$
 } else {
-
-  COUNT_DATA_DIR <- "/home/alma/ST-2018/CNNp/data/pre-tile-extraction"
-  FEATURE_FILE_DIR = '/home/alma/ST-2018/CNNp/DGE/data/test_data'
+  print("Run via terminal for proper activation.")
   
-  sample_names <- sort(list.files(COUNT_DATA_DIR, pattern = '*count*'))
-
-  # For test only
-  mat <- prepare_counts(COUNT_DATA_DIR,sample_names[1])
-  cdata <- prepare_coldata(FEATURE_FILE_DIR, sample_names[1])
-  
-  
-  sce <- SingleCellExperiment(list(counts = t(mat)))
-  sce <- computeSumFactors(sce)
-  sce = sce@int_colData@listData$size_factor
-  
-  # test
-  dds <- DESeqDataSetFromMatrix(countData = t(mat),
-                                colData = cdata,
-                                design = ~ f1)
-  sizeFactors(dds) <- sce
-  keep <- rowSums(counts(dds)) >= 40
-  dds <- dds[keep,]
-  dds <- DESeq(dds, parallel = TRUE)
-  res <-results(dds)
-  res <- results(dds, name="f1_tumor_vs_non.tumor")
-  res <- results(dds, contrast=c("f1","tumor","non-tumor"))
-  resultsNames(dds)
-  resLFC <- lfcShrink(dds, coef="f1_tumor_vs_non.tumor", type="apeglm")  
 }
